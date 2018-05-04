7ff0402b88b3c8476a89e865a947e7dd55981abe
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-05-04 09:06:12 -0700

    AJAX updates cart without page reload.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,10 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.new(item_params)
     @order.save
     session[:order_id] = @order.id
-    redirect_to products_path
+    respond_to do |format|
+      format.html { redirect_to products_path}
+      format.js { render :file => "layouts/create.js.erb" }
+    end
   end
 
   def update
