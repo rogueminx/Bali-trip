bd43e99a9cb25eab2bc49d48116ea0b6e44262ee
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-05-04 13:47:27 -0700

    add paperclip, update show.js to toggle, add photo for product to hidden div.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem "paperclip", "~> 6.0.0"
 gem 'rails', '~> 5.0.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_items
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
