7faf03c1d04169a20f19274336544207ed52b747
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-05-04 11:36:39 -0700

    flash greeting with user name at signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:notice] = "You've successfully signed up, #{user.name}!"
       redirect_to '/'
     else
       redirect_to '/signup'
